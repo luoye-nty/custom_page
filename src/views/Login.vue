c2d58e37d8265fb39246b96cb2165e878dc609cb
luoye <441374668@qq.com>
2020-07-24 17:35:49 +0800

    1

---
+++ src/views/Login.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <input type="text" v-model="loginForm.username" placeholder="用户名"/>
+        <input type="text" v-model="loginForm.password" placeholder="密码"/>
+        <button @click="login">登录</button>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+    export default {
+        data () {
+            return {
+                loginForm: {
+                    username: '',
+                    password: ''
+                }
+            };
+        },
+        methods: {
+            login () {
+                axios.defaults.headers.common["token"] = "baced";
+                axios.post("/api/userInfo/login",this.loginForm).then(response=>(
+                    console.log(response)
+                ))
+            }
+        }
+    };
+</script>
